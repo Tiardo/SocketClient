d8e1183d8f47683b75ddafe822a6588c0c57a043
DmitriyV <dima1999-91@mail.ru>
2024-08-14 13:19:59 +0300

    Добавьте файлы проекта.

---
+++ SocketClient/SocketClient.cpp
@@ -0,0 +1,19 @@
+﻿
+#define	WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+using namespace std;
+
+
+int main()
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+
+
+}
